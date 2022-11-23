5450cbf1fbb72260622afc208bc9b2ab124b3c9d
Klajdi Çaushi <klajdi.caushi95@gmail.com>
2022-11-23 22:35:39 +0100

    settings: removed ninja jwt settings

--- lajournal/settings/defaults.py
+++ lajournal/settings/defaults.py
@@ -95,8 +95,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-NINJA_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-}
