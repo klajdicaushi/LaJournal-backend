1615fe129fac1173d1244ba15208a6cd9577acbb
Klajdi Çaushi <klajdi.caushi@cardoai.com>
2026-01-06 22:18:19 +0100

    feat: added healthz

--- lajournal/urls.py
+++ lajournal/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path
 
 from project.api import api
 
+
+def healthz(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
     path("api/", api.urls),
     path("admin/", admin.site.urls),
+    path("healthz", healthz),
 ]
